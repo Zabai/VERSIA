c23781c5a30a7a2b578a16534918e0a9ea285350
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-03-12 16:13:57 +0000

    Routing Structure
    Routes needed to execute CRUD operations

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// INDEX
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// CREATE
+router.post('/', function(req, res, next) {
+    res.redirect('/');
+});
+
+// UPDATE
+router.put('/:id', function(req, res, next) {
+    res.redirect('/');
+});
+
+// DELETE
+router.delete('/:id', function(req, res, next) {
+    res.redirect('/');
 });
 
 module.exports = router;
