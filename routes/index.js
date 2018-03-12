24a651c119c0ab7d63927976ec35e497d68f8edb
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-03-12 19:02:08 +0000

    Routes operations
    CUD operations to routes

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,40 @@ router.get('/', function(req, res, next) {
 
 // CREATE
 router.post('/', function(req, res, next) {
+    var client = require('../db/client');
+
+    client.query("INSERT INTO todo(job) VALUES (?)", [req.body.job], function(error, results, fields) {
+        if(error)console.log("Error en el insert: " + error);
+        console.log("Resultado: " + JSON.stringify(results));
+        console.log("Fields: " + fields);
+    });
+
     res.redirect('/');
 });
 
 // UPDATE
 router.put('/:id', function(req, res, next) {
+    var client = require('../db/client');
+
+    client.query("UPDATE todo SET job = ? where id = ?", [req.body.job, req.params.id], function(error, results, fields) {
+        if(error)console.log("Error en el update: " + error);
+        console.log("Resultado: " + JSON.stringify(results));
+        console.log("Fields: " + fields);
+    });
+
     res.redirect('/');
 });
 
 // DELETE
 router.delete('/:id', function(req, res, next) {
+    var client = require('../db/client');
+
+    client.query("DELETE FROM todo WHERE id = ?", [req.params.id], function(error, results, fields) {
+        if(error)console.log("Error en el delete: " + error);
+        console.log("Resultado: " + JSON.stringify(results));
+        console.log("Fields: " + fields);
+    });
+
     res.redirect('/');
 });
 
