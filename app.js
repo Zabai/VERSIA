012c5f8fbdd504317b1667002df8b0b086c4fa20
Carlos Martel Lamas <carlos.martel105@alu.ulpgc.es>
2018-04-02 14:25:40 +0100

    now its done

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +17,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use(express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use(express.static(path.join(__dirname, 'node_modules/popper.js/dist')));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +39,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(8080);
 module.exports = app;
