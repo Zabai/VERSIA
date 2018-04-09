4544a1e4ffdab81544983a944db9062a26971102
Carlos Martel Lamas <carlos.martel105@alu.ulpgc.es>
2018-04-09 19:36:28 +0100

    register_user

--- config/passport.js
+++ config/passport.js
@@ -4,31 +4,122 @@ var LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
-}, function(email, password, cb) {
+}, function (email, password, cb) {
     var client = require('../db/db');
 
     client.query('SELECT * FROM User WHERE email=:email', {email: email},
-        function(err, user) {
-            if(err) return cb(err);
-            if(user.length === 0) return cb(null, false, { message: 'Usuario no encontrado.' });
-            if(user[0].password !== password) return cb(null, false, { message: 'Contraseña incorrecta.' });
+        function (err, user) {
+            if (err) return cb(err);
+            if (user.length === 0) return cb(null, false, {message: 'Usuario no encontrado.'});
+            if (user[0].password !== password) return cb(null, false, {message: 'Contraseña incorrecta.'});
 
             return cb(null, user[0]);
         });
 }));
 
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
     cb(null, user.email);
 });
 
-passport.deserializeUser(function(email, cb) {
+passport.deserializeUser(function (email, cb) {
     var client = require('../db/db');
 
-    client.query('SELECT * FROM User WHERE email=:email', {email: email},
-        function(err, user) {
-            if(err) return cb(err);
-            else cb(null, user[0]);
-        });
+    // find a user whose email is the same as the forms email
+    // we are checking to see if the user is trying to login already exists
+    connection.query("SELECT * FROM user WHERE email = ?", [email], function (err, rows) {
+        if (err)
+            return done(err);
+        if (rows.length) {
+            return done(null, false);
+        } else {
+            // if there is no user with that username
+            // create the user
+            var newUserMysql = {
+                email: email,
+                password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
+            };
+
+            var insertQuery = "INSERT INTO user ( email, password ) values (?,?)";
+
+            connection.query(insertQuery, [newUserMysql.email, newUserMysql.password], function (err, rows) {
+                newUserMysql.id = rows.insertId;
+
+                return done(null, newUserMysql);
+            });
+        }
+    });
+
 });
 
-module.exports = passport;
+// ========================================
+// LOCAL LOGIN
+// ========================================
+passport.use('local-login', new LocalStrategy({
+
+        usernameField: 'email',
+
+        passwordField: 'password',
+
+        passReqToCallback: true //passback entire req to call back
+    },
+    function (req, email, password, done) {
+
+
+        if (!email || !password) {
+            return done(null, false);
+        }
+
+        connection.query("SELECT * FROM user WHERE email = ?", [email], function (err, rows) {
+            if (err) {
+                return done(err);
+            }
+            if (!rows.length) {
+                return done(null, false);
+            }
+            // if the user is found but the password is wrong
+            //if (!bcrypt.compareSync(password, rows[0].password))
+            if (password !== rows[0].password)
+                return done(null, false);
+
+            // all is well, return successful user
+            return done(null, rows[0]);
+        });
+
+    }
+));
+
+// ========================================
+// LOCAL SIGNUP
+// ========================================
+passport.use('local-signup', new LocalStrategy({
+
+        usernameField: 'email',
+
+        passwordField: 'password',
+
+        passReqToCallback: true //passback entire req to call back
+    },
+    function (req, email, password, done) {
+
+        if (!email || !password) {
+            return done(null, false);
+        }
+        connection.query("SELECT * FROM user WHERE email = ?", [email], function (err, rows) {
+            if (err) {
+                return done(err);
+            }
+            if (rows.length) {
+                //if theres a user with that email, we do not permit him to register.
+                return done(null, false);
+            } else {
+                connection.query("INSERT INTO User(email, password) VALUES (:userEmail, :userPassword)", {userEmail: email, userPassword: password},
+                    function (err, newUser) {
+                        if (err) return done(null, false);
+                        else return done(null, newUser);
+                    });
+            }
+        });
+
+    }
+));
+
--- routes/friends.js
+++ routes/friends.js
@@ -14,7 +14,8 @@ router.post('/add', function (req, res, next) {
             console.log(err);
             return res.status(500).send({message: "Ha habido un error en la db: " + err});
         }
-        return res.status(200).send();
+        console.log(JSON.stringify(friendPetition));
+        return res.status(200).send(friendPetition);
     });
     dbConn.end()
 });
