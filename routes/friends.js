7539364e662ce483965718b2a2d6a8e08f04db4b
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-04-12 18:53:46 +0100

    Accept and decline friend petitions
    Logic and view, also added method override

--- routes/friends.js
+++ routes/friends.js
@@ -1,49 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     console.log(req.user);
 });
 
-router.post('/add', function (req, res, next) {
+router.post('/add', function(req, res, next) {
     var email = req.body.email;
 
     var client = require('../db/db');
-    client.query("INSERT INTO friends(sender, receiver) VALUES(:sender, :receiver)", {sender: req.user.email, receiver: email}, function(err, friendPetition){
-        if(err){
-            console.log(err);
-            return res.status(500).send({message: "Ha habido un error en la db: " + err});
-        }
-        return res.status(200).send();
-    });
+    client.query("INSERT INTO friends(sender, receiver) VALUES(:sender, :receiver)",
+        {sender: req.user.email, receiver: email},
+        function(err, friendPetition) {
+            if(err) {
+                console.log(err);
+                return res.status(500).send({message: "Ha habido un error en la db: " + err});
+            }
+            return res.status(200).send();
+        });
 
     client.end()
 });
 
-router.put('/accept',function(req, res, next) {
-    var recieverId = req.user.email; // Cambiar por el id o no
-    var senderId= req.query.email;
+router.put('/accept', function(req, res, next) {
+    var receiverId = req.user.email; // Cambiar por el id o no
+    var senderId = req.body.email;
 
     var client = require('../db/db');
-    client.query('UPDATE friends SET friend_request = 1 WHERE sender = :senderId AND reciever = :recieverId',
-        {senderId: senderId, recieverId: recieverId},
-        function (err, friendPetition) {
-            if (err) return res.status(500).send({message: "Ha habido un error en la db: " + err});
+    client.query('UPDATE friends SET friend_request = 1 WHERE sender = :senderId AND receiver = :receiverId',
+        {senderId: senderId, receiverId: receiverId},
+        function(err, friendPetition) {
+            if(err) return res.status(500).send({message: "Ha habido un error en la db: " + err});
             return res.status(200).send();
-    });
+        });
 
     client.end();
 });
 
-router.put('/decline',function(req, res, next) {
-    var recieverId = req.user.email; // Cambiar por el id o no
-    var senderId= req.query.email;
+router.delete('/decline', function(req, res, next) {
+    var receiverId = req.user.email; // Cambiar por el id o no
+    var senderId = req.body.email;
 
     var client = require('../db/db');
-    client.query('DELETE FROM friends WHERE sender = :senderId AND reciever = :recieverId',
-        {senderId: senderId, recieverId: recieverId},
-        function (err, friendPetition) {
-            if (err) return res.status(500).send({message: "Ha habido un error en la db: " + err});
+    client.query('DELETE FROM friends WHERE sender = :senderId AND receiver = :receiverId',
+        {senderId: senderId, receiverId: receiverId},
+        function(err, friendPetition) {
+            if(err) return res.status(500).send({message: "Ha habido un error en la db: " + err});
             return res.status(200).send();
         });
 
