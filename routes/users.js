855208f7f5723fdbe90d60cbf9f8739d12db14fe
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-04-29 17:27:03 +0100

    Working on students autocomplete search

--- routes/users.js
+++ routes/users.js
@@ -16,19 +16,29 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/search', function(req, res, next) {
-    //{req: req.query.search, loggedUserEmail: req.user.email}
     var client = require('../db/db');
-    var friendsQuery = "SELECT receiver, friend_request from friends where sender = :loggedUserEmail AND (friend_request=0 OR friend_request=1)";
-    var usersQuery = "SELECT email, name, surname, university, degree FROM profile WHERE profile.name LIKE :req AND profile.email <> :loggedUserEmail";
-    client.query(friendsQuery, {loggedUserEmail: req.user.email}, function(err, friends) {
-        if(err) return res.status(500).send({message: "Error en la petición cuando buscamos amigos, " + err});
-        console.log(JSON.stringify(friends));
-        client.query(usersQuery, {req: req.query.search, loggedUserEmail: req.user.email}, function(err, users) {
-            if(err) return res.status(500).send({message: "Error en la petición cuando buscamos usuarios, " + err});
-            console.log(JSON.stringify(users));
-            res.render("user/search", {users: users, friends: friends, search: req.query.search});
+
+    if(req.query.ajax) {
+        client.query("SELECT email, name, surname FROM profile WHERE name LIKE :name AND email <> :currentUser",
+            {name: req.query.search + "%", currentUser: req.user.email},
+            function(err, students) {
+                if(err) console.log(err);
+                res.json(JSON.stringify(students));
+            });
+    } else {
+        var friendsQuery = "SELECT receiver, friend_request from friends where sender = :loggedUserEmail AND (friend_request=0 OR friend_request=1)";
+        var usersQuery = "SELECT email, name, surname, university, degree FROM profile WHERE profile.name LIKE :req AND profile.email <> :loggedUserEmail";
+        client.query(friendsQuery, {loggedUserEmail: req.user.email}, function(err, friends) {
+            if(err) return res.status(500).send({message: "Error en la petición cuando buscamos amigos, " + err});
+            console.log(JSON.stringify(friends));
+            client.query(usersQuery, {req: req.query.search, loggedUserEmail: req.user.email}, function(err, users) {
+                if(err) return res.status(500).send({message: "Error en la petición cuando buscamos usuarios, " + err});
+                console.log(JSON.stringify(users));
+                res.render("user/search", {users: users, friends: friends, search: req.query.search});
+            });
         });
-    });
+    }
+
     client.end();
 });
 
@@ -43,7 +53,7 @@ router.get('/:id', function(req, res, next) {
 
     //values for the friend_list
     var friendRequests = {};
-    client.query("SELECT DISTINCT * FROM friends WHERE receiver=:loggedUserEmail AND friend_request=0", {loggedUserEmail: req.params.id}, function(err, friendRequestsRows){
+    client.query("SELECT DISTINCT * FROM friends WHERE receiver=:loggedUserEmail AND friend_request=0", {loggedUserEmail: req.params.id}, function(err, friendRequestsRows) {
         if(err) return res.status(500).send({message: "Ha habido un error en la db: " + err});
         friendRequests = friendRequestsRows;
     });
@@ -58,9 +68,13 @@ router.get('/:id', function(req, res, next) {
     client.end();
 });
 
-router.post('/:id/edit', function (req, res, next) {
-    var dbConn=require("../db/db");
-    dbConn.query("UPDATE profile SET name=:name, surname=:surname where email=:loggedEmail", {name: req.body.name, surname: req.body.surname, loggedEmail: req.user.email}, function(err, userUpdated){
+router.post('/:id/edit', function(req, res, next) {
+    var dbConn = require("../db/db");
+    dbConn.query("UPDATE profile SET name=:name, surname=:surname where email=:loggedEmail", {
+        name: req.body.name,
+        surname: req.body.surname,
+        loggedEmail: req.user.email
+    }, function(err, userUpdated) {
         if(err) return res.status(500).send({message: "Ha habido un error en la db: " + err});
         else if(userUpdated) {
             dbConn.query("UPDATE user SET email=:email WHERE email=:loggedEmail", {
